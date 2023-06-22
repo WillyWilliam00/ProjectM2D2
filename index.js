dc4e48f7aa49fbc713851b0d6edffcc1b5c593c7
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-06-23 00:30:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+//es 2//
+
+let num1 = 12
+let num2 = 20
+
+let somma = num1+num2
+
+
+console.log("Es2",somma)
+
+
+
+// es 3
+
+let x = 12
+
+console.log("Es3",x)
+
+// es 4
+
+let nome = "William"
+
+console.log("Es4",nome)
+
+// es 5 
+
+let y = 4 - x 
+let z = x - 4
+
+console.log("Es5",y,z)
+
+// es 6 
+
+let MeStesso = {
+    name:"William",
+    surname:"Costa",
+    hobby:"Coding",
+}
+
+
+console.log("Es6",MeStesso)
+
+// es 7 
+
+let name1 = "john"
+
+let name2 = "John"
+
+console.log("Es7",name1!==name2)
+
+name2 = name2.toLocaleLowerCase()
+
+console.log("Es7",name1!==name2)
