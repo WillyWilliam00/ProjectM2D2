8a07e621088727aca519a790e9d8199317762d64
WillyWilliam00 <costawilliam98@gmail.com>
2023-06-23 20:36:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ REGOLE
 
 innanzitutto spieghiamo cosa cono i datatype:
 
-Le variabili nel coding possono essere diverse da loro e tutte appartegono a diverse "categorie" in base appunto alla loro forma
+Le variabili nel coding possono essere diverse tra loro e tutte appartegono a diverse "categorie" in base appunto al loro tipo
 
 string = è una variabile "stringa", più semplicemente si tratta di una variabile che può contenere lettere e/o numeri, viene inserita tra " "
 ad esempio: "William" è una variabile stringa
@@ -36,7 +36,7 @@ undefined: è una variabile con valore non definito (come se prendessi un conten
 
 Quando dobbiamo raccogliere molte informazioni per elaborare un progetto complesso (come ad esempio l'anagrafica di qualcuno) possiamo utilizzare lo structural type Object,
 
-creiamo una variabile/costante con il nome del progetto e diamo delle coppie chiave-valore con tutte le informazioni che ci servono per quel determinato progetto.
+creiamo una variabile (o costante) con il nome del progetto e diamo delle coppie chiave-valore con tutte le informazioni che ci servono per quel determinato progetto.
 (chiave->nome di una info specifica valore-> appunto il valore di quella info) */
 
 const MeStesso = {
